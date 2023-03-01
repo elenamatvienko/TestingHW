d9bd690beedbba6e56790fe590544615882da0bb
elena <elena.matvienko2011@mail.ru>
2023-03-01 16:43:00 +0300

    commit

---
+++ src/main/java/me/matvienkoeg/testinghw/TestingHwApplication.java
@@ -0,0 +1,13 @@
+package me.matvienkoeg.testinghw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingHwApplication.class, args);
+	}
+
+}
